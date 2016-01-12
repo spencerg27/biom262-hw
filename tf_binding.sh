69486be28b6dc1c5617b81d461b5283c85fa7420
Spencer Goodman <spgoodm@ucsd.edu>
2016-01-12 10:36:08 -0800

    exercise 4 and 5 code

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,1 +1,7 @@
 cat tf.bed | awk '{if($4 =="NFKB") print}' > tf.nfkb.bed
+#EXERCISE2 
+#EXERCISE3 (include loading of biotools)
+
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+
