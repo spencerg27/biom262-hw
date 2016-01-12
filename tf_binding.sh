34a2def8b252789b1e0b6c3ce42fbba282e7dab8
Spencer Goodman <spgoodm@ucsd.edu>
2016-01-12 10:57:36 -0800

    placeholder comments

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,7 +1,7 @@
+#PBS TAGS
 cat tf.bed | awk '{if($4 =="NFKB") print}' > tf.nfkb.bed
 #EXERCISE2 
 #EXERCISE3 (include loading of biotools)
-
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
-
+#EXERCISE6
