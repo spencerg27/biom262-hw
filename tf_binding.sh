508774f03440327a03490dcacc84cb2f5bdc74da
Enching <e1luo@eng.ucsd.edu>
2016-01-14 11:17:57 -0800

    Add Exercise 4 and 5

--- tf_binding.sh
+++ tf_binding.sh
@@ -4,4 +4,8 @@ awk -F "\t" '{if($3 == "transcript") {print $0}}' gencode.v19.annotation.chr22.g
 #EXERCISE3 (include loading of biotools)
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+#Exercise 4: Use bedtools intersect to overlap TFs with promoter
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+#Exercise 5: Use bedtools getfasta to extract sequences
+bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s  -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 #EXERCISE6
